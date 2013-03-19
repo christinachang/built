0ce6a2d46a682a03b1f98fa67cf47a42956559ea
Yanik Jay <YanikJay@gmail.com>
2013-03-19 13:40:11 -0400

    add app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Built::Application.config.secret_token = '1e5d9a17ea3e1bf65b0001d86ed8d04b6e742a5b90baf9a9c14719bc765de911a890d6e14d8aa850a25fa9fdf5c5cb0ccb730af1e8774f5f4b87a47d8f064e1b'
