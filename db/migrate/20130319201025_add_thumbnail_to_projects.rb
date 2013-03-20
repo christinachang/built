7c8dfa674fcd5fffdda07efce2b91202f3f10a52
Yanik Jay <YanikJay@gmail.com>
2013-03-20 16:12:03 -0400

    delete migration file

--- db/migrate/20130319201025_add_thumbnail_to_projects.rb
+++
@@ -1,9 +0,0 @@
-class AddThumbnailToProjects < ActiveRecord::Migration
-  def self.up
-    add_attachment :projects, :thumbnail
-  end
-
-  def self.down
-    remove_attachment :projects, :thumbnail
-  end
-end
