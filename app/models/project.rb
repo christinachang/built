138b83d6f24725e25c7793672e139454aaf0ea34
Yanik Jay <YanikJay@gmail.com>
2013-03-21 14:31:29 -0400

    work on github methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,19 +8,43 @@ class Project < ActiveRecord::Base
   @@octokit_client = Octokit::Client.new(:login => "flatiron-001", 
                                           :password => "flatiron001")
 
+  def get_repo_hash(repo_name)
+    @@octokit_client.repo(repo_name)
+  end
+
   def get_html_url(repo_name)
-    repo_hash = @@octokit_client.repo(repo_name)
+    repo_hash = self.get_repo_hash(repo_name)
     repo_hash[:html_url]
   end
 
   def get_ssh_url(repo_name)
-    repo_hash = @@octokit_client.repo(repo_name)
+    repo_hash = self.get_repo_hash(repo_name)
     repo_hash[:ssh_url]
   end
 
   def get_description(repo_name)
-    repo_hash = @@octokit_client.repo(repo_name)
+    repo_hash = self.get_repo_hash(repo_name)
     repo_hash[:description]
   end
+
+  def get_collaborator_logins(repo_name)
+     repo_hash = @@octokit_client.collabs(repo_name)
+     repo_hash.collect do |collaborator|
+       {:login_name => collaborator.login}
+    end
+  end
+
+'JohnKellyFerguson/octomaps'
+
+  def get_fullnames_from_logins(repo_name)
+    logins = get_logins_from_repo_hash(repo_name)
+    logins.collect do |login|
+      @@octokit_client.user(login[:login_name]).name
+    end
+  end
+
+  [{:name => 'Jon', login =>'whatever:}, {}]
+
+  
 
 end
