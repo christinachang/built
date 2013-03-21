74f2078552f663e32277547b2065b35109f77e8e
Yanik Jay <YanikJay@gmail.com>
2013-03-21 11:41:28 -0400

    edited schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20130321141013) do
-
-  create_table "collaborators", :force => true do |t|
-    t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string   "login"
-  end
 
   create_table "images", :force => true do |t|
     t.string   "image_type"
