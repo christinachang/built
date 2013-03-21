07b951871489a800d4efa33e8d80da7a39bcf598
Christina Chang <christina.chang@flatironschool.com>
2013-03-21 11:06:54 -0400

    fix login flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,34 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
+
+  validates_presence_of :email
+  validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  validates_uniqueness_of :email, :case_sensitive => false
+  
+  validates_presence_of :password
+  # validates_confirmation_of :password
   has_secure_password
-  validates_uniqueness_of :email, :case_sesnsitive => false
+
+  # before_save :encrypt_password
+
+  # def encrypt_password
+  #   if password.present?
+  #     self.password_salt = BCrypt::Engine.generate_salt
+  #     self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  #   end
+  # end
+
+  # def self.authenticate(email, password)
+  #   user = find_by_email(email)
+  #   if user && user.password_hash == BCrypt::Engine.hash_secret(password, password_salt)
+  #     user
+  #   else
+  #     nil
+  #   end
+  # end
+
+def self.authenticate(email, password)
+    find_by_email(email).try(:authenticate, password)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Built::Application.routes.draw do
 
   resources :images
 
+  resources :sessions
 
   resources :projects
 
@@ -61,7 +62,7 @@ Built::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'projects#show'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
