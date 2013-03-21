76db689dcfe964743a23f449c4bd94243dc829b1
Justin Kestler <jkestler@gmail.com>
2013-03-21 10:11:42 -0400

    remove url column from images table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320200345) do
+ActiveRecord::Schema.define(:version => 20130321141013) do
+
+  create_table "collaborators", :force => true do |t|
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "login"
+  end
+
+  create_table "images", :force => true do |t|
+    t.string   "image_type"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.integer  "project_id"
+    t.string   "upload_file_name"
+    t.string   "upload_content_type"
+    t.integer  "upload_file_size"
+    t.datetime "upload_updated_at"
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -19,12 +37,8 @@ ActiveRecord::Schema.define(:version => 20130320200345) do
     t.text     "description"
     t.string   "url"
     t.string   "github_link"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
-    t.string   "thumbnail_file_name"
-    t.string   "thumbnail_content_type"
-    t.integer  "thumbnail_file_size"
-    t.datetime "thumbnail_updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "repo_name"
     t.string   "repo_url"
     t.string   "ssh_url"
