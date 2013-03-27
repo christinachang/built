c31dbab9e9720c931cd281b8e491a543f17dbf83
Justin Kestler <jkestler@gmail.com>
2013-03-27 15:45:40 -0400

    add profile edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> add profile edit form
 	def update
 		@user = current_user
 
