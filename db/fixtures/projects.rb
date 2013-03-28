3b630f87079e18e84594f49d92252009f6fb297e
Justin Kestler <jkestler@gmail.com>
2013-03-28 12:40:07 -0400

    update projects.rb, add to git

---
+++ db/fixtures/projects.rb
@@ -0,0 +1,14 @@
+Project.seed do |s|
+    s.id = 1
+    s.name = "rainbarf"
+    s.description = "A Ruby gem that makes rain look like vomit."
+    s.live_url = "http://www.rainbarf.com"
+    s.created_at = "04:03:02"
+    s.updated_at = "04:03:02"
+    s.repo_name = "creaktive/rainbarf"
+    s.repo_url = "http://www.github.com/creaktive/rainbarf"
+    s.ssh_url = "git@github.com/creaktive/rainbarf"
+    s.watchers = "4"
+    s.forks = "3"
+    s.language = "Ruby"
+end
