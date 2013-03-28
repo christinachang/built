1ca938fd1feb9a4dc4f75afd7b6fa56a123b18c3
Justin Kestler <jkestler@gmail.com>
2013-03-28 08:45:12 -0400

    continue working on seed_fu

--- db/fixtures/users.rb
+++ db/fixtures/users.rb
@@ -17,3 +17,81 @@ User.seed do |s|
   s.twitter_url = "http://www.twitter.com/frank"
 end
 
+User.seed do |s|
+  s.id    = 2
+  s.created_at = "03:03:30"
+  s.updated_at = "04:03:30"
+  s.github_login = "FredH"
+  s.password_token = "237508273450"
+  s.email = "fredh@fredh.com"
+  s.full_name = "Frederick Handy"
+  s.github_html_url = "http://www.github.com/fredh"
+  s.token = "w19837240q87wr08a97r"
+  s.profile_image_file_name = "fredh.jpg"
+  s.profile_image_content_type = "jpeg"
+  s.profile_image_file_size = "454k"
+  s.profile_image_updated_at = "03:03:30"
+  s.linkedin_url = "http://www.linkedin.com/fredh"
+  s.facebook_url = "http://www.facebook.com/fredh"
+  s.twitter_url = "http://www.twitter.com/fredh"
+end
+
+User.seed do |s|
+  s.id    = 3
+  s.created_at = "03:03:30"
+  s.updated_at = "04:03:30"
+  s.github_login = "wackojacko"
+  s.password_token = "2375082343450"
+  s.email = "jack@jack.com"
+  s.full_name = "Jack O. Nassis"
+  s.github_html_url = "http://www.github.com/wackojacko"
+  s.token = "w19837240q87wr0234234r"
+  s.profile_image_file_name = "jacko.jpg"
+  s.profile_image_content_type = "jpeg"
+  s.profile_image_file_size = "452k"
+  s.profile_image_updated_at = "03:03:30"
+  s.linkedin_url = "http://www.linkedin.com/jack"
+  s.facebook_url = "http://www.facebook.com/jack"
+  s.twitter_url = "http://www.twitter.com/jack"
+end
+
+User.seed do |s|
+  s.id    = 4
+  s.created_at = "03:03:30"
+  s.updated_at = "04:03:30"
+  s.github_login = "shybird"
+  s.password_token = "237523423482343450"
+  s.email = "bird@bird.com"
+  s.full_name = "Bird Asa Feather"
+  s.github_html_url = "http://www.github.com/bird"
+  s.token = "w19837240q87wr0234234r"
+  s.profile_image_file_name = "birdo.jpg"
+  s.profile_image_content_type = "jpeg"
+  s.profile_image_file_size = "552k"
+  s.profile_image_updated_at = "03:03:30"
+  s.linkedin_url = "http://www.linkedin.com/bird"
+  s.facebook_url = "http://www.facebook.com/bird"
+  s.twitter_url = "http://www.twitter.com/bird"
+end
+
+User.seed do |s|
+  s.id    = 5
+  s.created_at = "03:03:30"
+  s.updated_at = "04:03:30"
+  s.github_login = "littlepiggy"
+  s.password_token = "237523423482343450"
+  s.email = "piggy@bacon.com"
+  s.full_name = "Richard Ashcroft"
+  s.github_html_url = "http://www.github.com/littlepiggy"
+  s.token = "w19837240q87wr0234234r"
+  s.profile_image_file_name = "piggy.jpg"
+  s.profile_image_content_type = "jpeg"
+  s.profile_image_file_size = "552k"
+  s.profile_image_updated_at = "03:03:30"
+  s.linkedin_url = "http://www.linkedin.com/littlepiggy"
+  s.facebook_url = "http://www.facebook.com/littlepiggy"
+  s.twitter_url = "http://www.twitter.com/littlepiggy"
+end
+
+
+
