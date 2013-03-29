1530880a82e50b7ce0808ce42b80a23d7e85ff7c
Christina Chang <christina.chang@flatironschool.com>
2013-03-29 15:57:59 -0400

    confirm validation of social media link fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ActiveRecord::Base
   :default_url => "/images/:style/missing.png"
 
   validates :full_name, :presence => {:message => 'please enter full name'}
-  # validates_format_of :github_html_url, :with => /github/, :message => "please provide an accurate github url"
-  # validates_format_of :linkedin_url, :with => /linkedin/, :message => "please provide an accurate linkedin url"
-  # validates_format_of :facebook_url, :with => /facebook/, :message => "please provide an accurate facebook url"
-  # validates_format_of :twitter_url, :with => /twitter/, :message => "please provide an accurate twitter url"
+  validates_format_of :github_html_url, :with => /github/, :message => "please provide an accurate github url"
+  validates_format_of :linkedin_url, :allow_blank => true, :with => /linkedin/, :message => "please provide an accurate linkedin url"
+  validates_format_of :facebook_url, :allow_blank => true, :with => /facebook/, :message => "please provide an accurate facebook url"
+  validates_format_of :twitter_url, :allow_blank => true, :with => /twitter/, :message => "please provide an accurate twitter url"
 
   @@admin = ["meowist", "christinachang","modernlegend","anabecker"]
 
