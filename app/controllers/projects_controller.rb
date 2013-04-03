9495e9e55ce7b3ae69d85e7779c28c7d7d438878
Justin Kestler <jkestler@gmail.com>
2013-04-03 16:03:30 -0400

    refactor project new to improve front-end validations, add project delete confirm to profile edit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -95,7 +95,7 @@ end
     @project.destroy
 
     respond_to do |format|
-      format.html { redirect_to projects_url }
+      format.html { redirect_to current_user }
       format.json { head :no_content }
     end
   end
