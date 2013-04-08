8bb8d3ac4fcc156c8c7b51c9cedcbcc4f8bb1059
aviflombaum <avi@flombaum.com>
2013-04-08 14:58:20 -0400

    fixed migrations with paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130401175057) do
 
-  create_table "cover_photos", :force => true do |t|
-    t.integer  "project_id"
-    t.string   "upload_file_name"
-    t.string   "upload_content_type"
-    t.integer  "upload_file_size"
-    t.datetime "upload_updated_at"
-  end
-
   create_table "images", :force => true do |t|
     t.string   "image_type"
     t.datetime "created_at",          :null => false
@@ -41,7 +33,8 @@ ActiveRecord::Schema.define(:version => 20130401175057) do
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.string   "live_url"
+    t.string   "url"
+    t.string   "github_link"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "repo_name"
