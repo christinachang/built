20d085e892ce5eb4eb26681e9944672083460ce2
Yanik Jay <YanikJay@gmail.com>
2013-04-08 12:21:46 -0400

    fix anonymous name and user submit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@ class User < ActiveRecord::Base
 
   @@admin = ["meowist", "christinachang","modernlegend","anabecker"]
 
+  def has_full_name?
+    self.full_name.downcase.capitalize == 'Anonymous'
+  end
+  
   def is_authorized?(params)
     self.is_an_admin? || self.is_a_member_of_this_project?(params)
   end 
