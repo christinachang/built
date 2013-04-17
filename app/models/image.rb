4a7b5a9fea3402c9b345c421859e2a0a29c3a1db
Christina Chang <christina.chang@flatironschool.com>
2013-04-17 15:44:12 -0400

    add validation for image file size

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,6 +9,7 @@ class Image < ActiveRecord::Base
   validates :upload_file_size, :presence => { :message => "Please upload at least one screenshot" }, :if => lambda { |u| u.image_type=="screenshot" }
   validates :upload_file_size, :presence => { :message => "Please upload a cover image" }, :if => lambda { |u| u.image_type=="cover" }
   #validates :upload_file_size, :less_than => 2.megabytes #=> { :message => "Images cannot be more than 2 megabytes in size" }
+  validates_attachment_size :upload, :less_than => 2.megabytes
 
 end
 
