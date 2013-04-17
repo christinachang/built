d67347653a49b15dd2b3fa526cea66d5f95049cf
Yanik Jay <YanikJay@gmail.com>
2013-04-17 14:35:07 -0400

    fix view for chart

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -29,6 +29,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
 
+
     client = Octokit::Client.new(:login => current_user.github_login, :oauth_token => current_user.token, :auto_traversal => true)
     data_structure = client.commits(@project.repo_name)
     @data_hash = {}
@@ -78,7 +79,51 @@ end
 
 
 
+####################### second method #########################
+
+    @data_array = []
+  
+    data_structure.each do |instance|
+        
+      commit_time = instance.commit.author.date.to_datetime.in_time_zone("Eastern Time (US & Canada)")
+      if commit_time.min >= 30
+      commit_hour = commit_time.beginning_of_hour
+      else 
+      commit_hour = commit_time.end_of_hour
+      end
+      @data_array << commit_hour.strftime("%l%P").strip.to_sym
+    end
+
+     day_hour_array = (0..23).to_a
+     @count = {}
+     day_hour_array.each do |element|
+      hour = DateTime.parse(element.to_s + ":00").strftime("%l%P").strip.to_sym
+      @count[hour] = 0
+     end
+    
+
+     @total = 0.to_f
+     @data_array.sort.each do |value| 
+      @count[value] ||= 0
+      @count[value] += 1    
+      @total += 1              
+    end
+
+      @count.each do |key, value|
+      @count[key] = ((value/@total) * 100).round(2)
+   end
+   @percent_array = []
+   @hour_array = []
+   @count.each do |k,v|
+   @hour_array << k
+   @percent_array << v
+ end
+
+#########################
+
+
 end
+
 
   # GET /projects/new
   # GET /projects/new.json
