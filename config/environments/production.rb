dc3743e4b62df61f21a5efe86636c08b8f518e94
Ana Becker <anabdesigns@gmail.com>
2013-04-24 22:23:48 -0400

    added actionmailer config stuff to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,19 @@ Built::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.middleware.use ExceptionNotifier,
+    sender_address: 'builtexceptions@gmail.com',
+    exception_recipients: ['anabdesigns@gmail.com', 'builtexceptions@gmail.com']
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'built.flatironschool.com',
+    :user_name            => ENV['EMAIL_USERNAME'],
+    :password             => ENV['EMAIL_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+
 end
